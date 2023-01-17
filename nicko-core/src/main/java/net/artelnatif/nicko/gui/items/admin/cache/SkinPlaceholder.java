f3284d3c05d2d1fefaf79bb264671e7634905e4f
aro <pantoine.rochas@gmail.com>
2023-01-17 01:04:10 +0100

    fix: translate to UUID

--- nicko-core/src/main/java/net/artelnatif/nicko/gui/items/admin/cache/SkinPlaceholder.java
+++ nicko-core/src/main/java/net/artelnatif/nicko/gui/items/admin/cache/SkinPlaceholder.java
@@ -5,12 +5,14 @@ import de.studiocode.invui.item.builder.SkullBuilder;
 import de.studiocode.invui.item.impl.AsyncItem;
 import org.bukkit.Material;
 
+import java.util.UUID;
+
 public class SkinPlaceholder extends AsyncItem {
     public SkinPlaceholder(String name) {
         super(new ItemBuilder(Material.PAINTING).setDisplayName("§7§oLoading..."), () -> {
-            System.out.println("name = " + name);
-            final SkullBuilder skull = new SkullBuilder(name);
-            skull.setDisplayName(name);
+            String uuid = name.replaceAll("(.{8})(.{4})(.{4})(.{4})(.+)", "$1-$2-$3-$4-$5");
+            final SkullBuilder skull = new SkullBuilder(UUID.fromString(uuid));
+            skull.setDisplayName("Skin");
             skull.addLoreLines("§7Click to invalidate skin");
             return skull;
         });

aba24384d54489745076184114f660c3c68fcc83
aro <pantoine.rochas@gmail.com>
2023-01-19 19:07:58 +0100

    refactor: extract uuid to var

--- nicko-core/src/main/java/net/artelnatif/nicko/gui/items/admin/cache/SkinPlaceholder.java
+++ nicko-core/src/main/java/net/artelnatif/nicko/gui/items/admin/cache/SkinPlaceholder.java
@@ -10,9 +10,10 @@ import java.util.UUID;
 public class SkinPlaceholder extends AsyncItem {
     public SkinPlaceholder(String name) {
         super(new ItemBuilder(Material.PAINTING).setDisplayName("§7§oLoading..."), () -> {
-            String uuid = name.replaceAll("(.{8})(.{4})(.{4})(.{4})(.+)", "$1-$2-$3-$4-$5");
-            final SkullBuilder skull = new SkullBuilder(UUID.fromString(uuid));
-            skull.setDisplayName("Skin");
+            final String stringUUID = name.replaceAll("(.{8})(.{4})(.{4})(.{4})(.+)", "$1-$2-$3-$4-$5");
+            final UUID uuid = UUID.fromString(stringUUID);
+            final SkullBuilder skull = new SkullBuilder(uuid);
+            skull.setDisplayName("§6Skin Entry");
             skull.addLoreLines("§7Click to invalidate skin");
             return skull;
         });

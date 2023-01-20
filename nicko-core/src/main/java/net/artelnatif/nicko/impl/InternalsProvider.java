f7a9689ee27cd6f3208b2aeeba15c7d264091d89
aro <pantoine.rochas@gmail.com>
2023-01-20 16:53:35 +0100

    refactor: remove debug statements

--- nicko-core/src/main/java/net/artelnatif/nicko/impl/InternalsProvider.java
+++ nicko-core/src/main/java/net/artelnatif/nicko/impl/InternalsProvider.java
@@ -12,9 +12,6 @@ public class InternalsProvider {
             final String packageName = Internals.class.getPackage().getName();
             final String bukkitVersion = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
             final String fullClassName = packageName + "." + bukkitVersion;
-            System.out.println("[DEBUG] packageName = " + packageName);
-            System.out.println("[DEBUG] bukkitVersion = " + bukkitVersion);
-            System.out.println("[DEBUG] Searching for " + fullClassName + "...");
             internals = (Internals) Class.forName(fullClassName).getConstructors()[0].newInstance();
         } catch (InvocationTargetException | ClassNotFoundException | InstantiationException | IllegalAccessException |
                  ClassCastException exception) {

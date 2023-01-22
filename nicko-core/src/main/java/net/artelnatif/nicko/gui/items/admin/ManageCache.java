8634f4c84ceb2bea1a84ae6a5738ab0fbe7d5849
aro <pantoine.rochas@gmail.com>
2023-01-22 15:27:20 +0100

    feat: make item async

--- nicko-core/src/main/java/net/artelnatif/nicko/gui/items/admin/ManageCache.java
+++ nicko-core/src/main/java/net/artelnatif/nicko/gui/items/admin/ManageCache.java
@@ -1,21 +1,26 @@
 package net.artelnatif.nicko.gui.items.admin;
 
-import de.studiocode.invui.item.ItemProvider;
+import de.studiocode.invui.item.builder.ItemBuilder;
 import de.studiocode.invui.item.builder.SkullBuilder;
-import de.studiocode.invui.item.impl.BaseItem;
+import de.studiocode.invui.item.impl.AsyncItem;
 import net.artelnatif.nicko.gui.admin.CacheManagementGUI;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.jetbrains.annotations.NotNull;
 
-public class ManageCache extends BaseItem {
-    @Override
-    public ItemProvider getItemProvider() {
-        final SkullBuilder builder = new SkullBuilder("Notch");
-        builder.setDisplayName("§fManage §6skin §fcache...");
-        builder.addLoreLines("§7Access the skin cache management panel.");
-        return builder;
+public class ManageCache extends AsyncItem {
+    public ManageCache() {
+        super(new ItemBuilder(Material.PAINTING)
+                        .setDisplayName("§fManage §6skin §fcache...")
+                        .addLoreLines("§7Access the skin cache management panel."),
+                () -> {
+                    final SkullBuilder builder = new SkullBuilder("Notch");
+                    builder.setDisplayName("§fManage §6skin §fcache...");
+                    builder.addLoreLines("§7Access the skin cache management panel.");
+                    return builder;
+                });
     }
 
     @Override

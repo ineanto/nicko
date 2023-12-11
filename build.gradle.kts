e5ce8bde1abefb07381779232741436c593ee803
ineanto <pantoine.rochas@gmail.com>
2023-12-11 12:31:16 +0100

    fix(build): includes/excludes fix (1.0.5-RC1)

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,16 +39,16 @@ repositories {
 
 dependencies {
     implementation("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
-    implementation("me.clip:placeholderapi:2.11.4")
     implementation("com.comphenix.protocol:ProtocolLib:5.1.1-SNAPSHOT")
 
+    shadowImplementation("me.clip:placeholderapi:2.11.4")
     shadowImplementation("xyz.xenondevs.invui:invui:1.23")
     shadowImplementation("net.wesjd:anvilgui:1.9.0-SNAPSHOT")
     shadowImplementation("com.github.jsixface:yamlconfig:1.2")
     shadowImplementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.15.2")
     shadowImplementation("com.fasterxml.jackson.core:jackson-core:2.15.2")
     shadowImplementation("com.mysql:mysql-connector-j:8.1.0")
-    shadowImplementation("org.mariadb.jdbc:mariadb-java-client:3.1.2")
+    shadowImplementation("org.mariadb.jdbc:mariadb-java-client:3.3.1")
     shadowImplementation("redis.clients:jedis:4.4.3")
     shadowImplementation("com.google.code.gson:gson:2.10.1")
 
@@ -76,15 +76,9 @@ tasks {
 
         configurations = listOf(shadowImplementation)
 
-        // EXCLUSIONS
-        dependencies {
-            exclude("io.papermc.paper:paper-api:.*")
-            exclude("me.clip:placeholderapi:.*")
-            exclude("com.comphenix.protocol:ProtocolLib:.*")
-        }
-
         // RELOCATIONS
         relocate("xyz.xenondevs", "xyz.ineanto.nicko.libs.invui")
+        relocate("me.clip", "xyz.ineanto.nicko.libs.placeholderapi")
         relocate("net.wesjd", "xyz.ineanto.nicko.libs.anvilgui")
         relocate("com.github.jsixface", "xyz.ineanto.nicko.libs.yaml")
         relocate("com.fasterxml.jackson.dataformat", "xyz.ineanto.nicko.libs.jackson.yaml")
@@ -92,19 +86,24 @@ tasks {
         relocate("com.mysql", "xyz.ineanto.nicko.libs.mysql")
         relocate("org.mariadb.jdbc", "xyz.ineanto.nicko.libs.mariadb")
         relocate("redis.clients", "xyz.ineanto.nicko.libs.redis")
+        relocate("com.google.gson", "xyz.ineanto.nicko.libs.gson")
         relocate("org.apache.commons.pool2", "xyz.ineanto.nicko.libs.pool2")
-        relocate("com.google.code.gson", "xyz.ineanto.nicko.libs.gson")
 
-        // MINIMIZING
-        minimize {
-            exclude(dependency("net.wesjd:.*"))
-            exclude(dependency("xyz.xenondevs.invui:.*"))
-            exclude("colors.bin")
-            exclude("org/**")
-            exclude("waffle/**")
-            exclude("google/**")
-            exclude("com/sun/**")
-        }
+        exclude("colors.bin")
+        exclude("waffle/**")
+        exclude("com/sun/**")
+        exclude("com/google/protobuf/**")
+        exclude("com/google/errorprone/**")
+        exclude("org/apache/commons/logging/**")
+        exclude("org/bstats/**")
+        exclude("org/jetbrains/**")
+        exclude("org/intellij/**")
+        exclude("org/checkerframework/**")
+        exclude("org/json/**")
+        exclude("org/slf4j/**")
+        exclude("org/yaml/**")
+        exclude("google/protobuf/**")
+        exclude("net/kyori/**")
     }
 }
 

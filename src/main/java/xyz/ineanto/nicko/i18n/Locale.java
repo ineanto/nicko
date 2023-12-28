af4625793cdcae3403c474dd74433524ba56184d
ineanto <pantoine.rochas@gmail.com>
2023-12-28 21:40:35 +0100

    fix(i18n): bump locale version

--- src/main/java/xyz/ineanto/nicko/i18n/Locale.java
+++ src/main/java/xyz/ineanto/nicko/i18n/Locale.java
@@ -9,7 +9,7 @@ public enum Locale implements Serializable {
     FRENCH("fr", "Français"),
     CUSTOM("cm", "Server Custom");
 
-    public static final Version VERSION = new Version(1, 1, 0);
+    public static final Version VERSION = new Version(1, 1, 1);
 
     private final String code;
     private transient final String name;
